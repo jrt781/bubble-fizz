519db2b75e6ed817762786f7c81ad393c1904c58
Jake Tyler <jrt@byu.edu>
2018-03-01 14:03:53 -0700

    basic game functionality

--- script.js
+++ script.js
@@ -1,89 +1,43 @@
 var app = new Vue({
-  el: '#app',
+  el: '#game',
   data: {
-    number: '',
-    max: '',
-    current: {},
-    loading: true,
-    addedName: '',
-    addedComment: '',
-    comments: {},
-    ratings: {},
-  },
-  created: function() {
-    this.xkcd();
-  },
-  computed: {
-    month: function() {
-      var month = new Array;
-      if (this.current.month === undefined)
-        return '';
-      month[0] = "January";
-      month[1] = "February";
-      month[2] = "March";
-      month[3] = "April";
-      month[4] = "May";
-      month[5] = "June";
-      month[6] = "July";
-      month[7] = "August";
-      month[8] = "September";
-      month[9] = "October";
-      month[10] = "November";
-      month[11] = "December";
-      return month[this.current.month - 1];
-    },
-    averageRating: function() {
-      if (!(this.number in this.ratings))
-        return "Not yet rated. Be the first!";
-      var average = this.ratings[this.number].sum/this.ratings[this.number].number;
-      var roundedAverage = average.toFixed(1);
-      return roundedAverage;
-    }
-  },
-  watch: {
-    number: function(value,oldvalue) {
-      if (oldvalue === '') {
-	      this.max = value;
-      } else {
-	      this.xkcd();
-      }
-    },
+    gameOn: false,
+    score: 0,
+    bubbles: [{x:300,y:100,size:'250',color:'red'},
+    {x:700,y:200,size:'150',color:'green'},
+    {x:600,y:200,size:'40',color:'blue'},
+    {x:375,y:230,size:'97',color:'green'},
+    {x:256,y:836,size:'150',color:'cyan'},
+            ],
   },
   methods: {
-    xkcd: function() {
-      this.loading = true;
-      fetch('https://xkcd.now.sh/' + this.number).then(response => {
-        return response.json();
-      }).then(json => {
-        this.current = json;
-        this.loading = false;
-        this.number = json.num;
-        console.log(json);
-        return true;
-      }).catch(err => {
-        this.number = this.max;
-      });
+    popBubble: function(index) {
+      this.score++;
+      this.bubbles.splice(index,1);
+      if (this.bubbles.length == 0) {
+        this.gameOn = false;
+      }
     },
-    previousComic: function() {
-      this.number = this.current.num - 1;
+    
+    startGame: function() {
+      this.bubbles = [];
+      for (var i = 0; i < 10; i++) {
+        var bubble = {};
+        bubble.x = this.getRandom(0, $(window).width());
+        bubble.y = this.getRandom(0, $(window).height());
+        bubble.size = this.getRandom(10, 250);
+        bubble.color = "green";
+        this.bubbles.push(bubble);
+      }
+      this.gameOn = true;
     },
-    nextComic: function() {
-      this.number = this.current.num + 1;
-    },
-    firstComic: function() {
-      this.number = 1;
-    },
-    lastComic: function() {
-      this.number = this.max;
-    },
+
     getRandom: function(min, max) {
       min = Math.ceil(min);
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum and minimum are inclusive 
     },
-    randomComic: function() {
-      this.number = this.getRandom(1, this.max);
-    },
+    
     addComment: function() {
       if (!(this.number in this.comments)) {
 	      Vue.set(app.comments, this.number, new Array);
@@ -95,13 +49,5 @@ var app = new Vue({
       this.addedName = '';
       this.addedComment = '';
     },
-    setRating: function(rating){
-      if (!(this.number in this.ratings))
-        Vue.set(this.ratings,this.number,{sum:0,number:0});
-      this.ratings[this.number].sum += rating;
-      this.ratings[this.number].number += 1;
-    }
   }
 });
-
-Vue.component('star-rating', VueStarRating.default);
