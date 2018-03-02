a234f5ed6495230c8bf5f444da40ecff0373862c
Jake Tyler <jrt@byu.edu>
2018-03-01 23:37:25 -0700

    instructions, color changes

--- script.js
+++ script.js
@@ -12,30 +12,61 @@ var app = new Vue({
     bestScore: 0,
     scoreYet: false,
     gameDone: false,
+    buttonBackgroundColor: "black",
+    buttonTextColor: "white",
+    tooltipBackgroundColor: "black",
+    tooltipTextColor: "white",
   },
   created() {
     window.addEventListener('keyup', this.backToHomeScreen);
+    
     var random_number = Math.floor(Math.random() * this.colors.length);
     var random_color = this.colors[random_number];
-    $('#startGameButton').css('background-color', random_color);
+    this.buttonBackgroundColor = random_color;
     if (random_number % 2 == 0) {
-      $('#startGameButton').css('color', "white");
+      this.buttonTextColor = "white";
     } else {
-      $('#startGameButton').css('color', "black");
+      this.buttonTextColor = "black";
     }
+    
+    var random_number2 = Math.floor(Math.random() * this.colors.length);
+    while (random_number2 == random_number) {
+      random_number2 = Math.floor(Math.random() * this.colors.length);
+    }
+    var random_color2 = this.colors[random_number2];
+    this.tooltipBackgroundColor = random_color2;
+    if (random_number2 % 2 == 0) {
+      this.tooltipTextColor = "white";
+    } else {
+      this.tooltipTextColor = "black";
+    }
+    
   },
   methods: {
     backToHomeScreen: function(event) {
       if (event.which == 32 && this.gameDone) {
         this.gameDone = false;
         this.gameOn = false;
+        
         var random_number = Math.floor(Math.random() * this.colors.length);
         var random_color = this.colors[random_number];
-        $('#startGameButton').css('background-color', random_color);
+        this.buttonBackgroundColor = random_color;
         if (random_number % 2 == 0) {
-          $('#startGameButton').css('color', "white");
+          this.buttonTextColor = "white";
         } else {
-          $('#startGameButton').css('color', "black");
+          this.buttonTextColor = "black";
+        }
+        
+        var random_number2 = Math.floor(Math.random() * this.colors.length);
+        while (random_number2 == random_number) {
+          random_number2 = Math.floor(Math.random() * this.colors.length);
+        }
+        var random_color2 = this.colors[random_number2];
+        this.tooltipBackgroundColor = random_color2;
+        if (random_number2 % 2 == 0) {
+          this.tooltipTextColor = "white";
+        } else {
+          this.tooltipTextColor = "black";
         }
       }
     },
