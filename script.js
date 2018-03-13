04296c082196a049858adb80c01d2811ffedc0b5
Jake Tyler <jrt@byu.edu>
2018-03-13 00:04:54 -0600

    fizz functionality

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var app = new Vue({
     colors: ["#FF0000", "#00FF00", "#0000FF", "#FFFF00",
              "#FF00FF", "#00FFFF", "#FF4488", "#00CC66",
              "#6600CC", "#44FF88", "#4488FF", "#FFAA22"],
+    timerDisplay: 0,
     timer: 0,
     interval: 0,
     score: 0,
@@ -35,11 +36,14 @@ var app = new Vue({
         bubble.size = this.getRandom(25, 250);
         bubble.x = this.getRandom(0, $(window).width()-bubble.size);
         bubble.y = this.getRandom(0, $(window).height()-bubble.size);
+        bubble.dx = this.getRandomFloat(-5, 5);
+        bubble.dy = this.getRandomFloat(-5, 5);
         bubble.color = this.colors[this.getRandom(1, 12)];
       }
     },
     
-    startBubbles: function() {
+    startBubbles: function(gameType) {
+      this.gameType = gameType;
       this.bubbles = [];
       this.score = 0;
       for (var i = 0; i < 10; i++) {
@@ -47,6 +51,8 @@ var app = new Vue({
         bubble.size = this.getRandom(25, 250);
         bubble.x = this.getRandom(0, $(window).width()-bubble.size);
         bubble.y = this.getRandom(0, $(window).height()-bubble.size);
+        bubble.dx = this.getRandomFloat(-3, 3);
+        bubble.dy = this.getRandomFloat(-3, 3);
         bubble.color = this.colors[this.getRandom(1, 12)];
         this.bubbles.push(bubble);
       }
@@ -54,15 +60,35 @@ var app = new Vue({
       var v = this;
       this.timer = 15;
       this.interval = setInterval(function () {
-        v.timer -= 0.1;
+        v.timer -= 0.015;
+        
+        if (v.gameType == "fizz") {
+          for (var i = 0; i < v.bubbles.length; i++) {
+            v.bubbles[i].x += v.bubbles[i].dx;
+            v.bubbles[i].y += v.bubbles[i].dy;
+            if (v.bubbles[i].x > $(window).width()-bubble.size) {
+              v.bubbles[i].dx *= -1;
+            }
+            if (v.bubbles[i].x < 0) {
+              v.bubbles[i].dx *= -1;
+            }
+            if (v.bubbles[i].y > $(window).height()-bubble.size) {
+              v.bubbles[i].dy *= -1;
+            }
+            if (v.bubbles[i].y < 0) {
+              v.bubbles[i].dy *= -1;
+            }
+          }
+        }
+        
         if (v.timer <= 0) {
           v.timer = 0;
           clearInterval(v.interval);
           v.bestScore = Math.max(v.bestScore, v.score);
           v.gameDone = true;
         }
-        v.timer = v.timer.toFixed(1);
-      }, 100);
+        v.timerDisplay = v.timer.toFixed(1);
+      }, 10);
       this.gameOn = true;
     },
 
@@ -70,6 +96,10 @@ var app = new Vue({
       min = Math.ceil(min);
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum and minimum are inclusive 
+    },
+
+    getRandomFloat: function(min, max) {
+      return Math.random() * (max - min + 1) + min; //The maximum and minimum are inclusive 
     },
   }
 });
