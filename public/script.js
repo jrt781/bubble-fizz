4e4d3da5877a06576f3dc8ecf40b288fe3c8aa55
Jake Tyler <jrt@byu.edu>
2018-03-14 22:57:37 -0600

    node and top ten initial functionality

--- public/script.js
+++ public/script.js
@@ -6,13 +6,15 @@ var app = new Vue({
     colors: ["#FF0000", "#00FF00", "#0000FF", "#FFFF00",
              "#FF00FF", "#00FFFF", "#FF4488", "#00CC66",
              "#6600CC", "#44FF88", "#4488FF", "#FFAA22"],
+    randomWords: ['try', 'gorgeous', 'company', 'command', 'hydrant', 'nation', 'pigs', 'fallacious', 'salt', 'mailbox', 'military', 'brake', 'alike', 'object', 'high-pitched', 'rhythm', 'cycle', 'wistful', 'tart', 'vase', 'remain', 'committee', 'bat', 'divide', 'value', 'team', 'alive', 'church', 'cheat', 'play', 'carpenter', 'part', 'competition', 'coach', 'connection', 'nutty', 'clean', 'bright', 'knowledge', 'thinkable', 'new', 'knock', 'mindless', 'industrious', 'brick', 'typical', 'wing', 'meaty', 'late', 'route', 'sable', 'normal', 'aunt', 'dare', 'plantation', 'straight', 'ablaze', 'self', 'spot', 'pickle', 'damage', 'fail', 'wild', 'experience', 'recognise', 'poke', 'scarce', 'knot', 'sail', 'disgusted', 'digestion', 'warm', 'structure', 'understood', 'available', 'fork', 'itchy', 'languid', 'hurry', 'needless', 'misty', 'sign', 'tongue', 'hover', 'temper', 'motionless', 'bee', 'house', 'decision', 'cows', 'handy', 'reading', 'squeamish', 'admire', 'permissible', 'pear', 'birds', 'acid', 'possible', 'hesitant', 'adjoining', 'harm', 'slow', 'addition', 'coherent', 'fretful', 'bawdy', 'heartbreaking', 'chew', 'big', 'scrawny', 'sugar', 'back', 'push', 'toothsome', 'slope', 'slap', 'kick', 'fetch', 'mitten', 'jewel', 'pinch', 'consist', 'spiders', 'used', 'blood', 'regret', 'oafish', 'naive', 'wind', 'attack', 'rich', 'fang', 'uneven', 'wrench', 'vegetable', 'tight', 'ossified', 'songs', 'swim', 'orange', 'motion', 'supreme', 'like', 'disastrous', 'depressed', 'chubby', 'note', 'rock', 'marry', 'pour', 'smelly', 'explain', 'interfere', 'humor', 'deeply', 'rainstorm', 'crime', 'blushing', 'feeling', 'smoggy', 'move', 'wall', 'train', 'thoughtful', 'shrug', 'shaky', 'mushy', 'debonair', 'middle', 'abnormal', 'shop', 'hurried', 'forgetful', 'attack', 'defeated', 'shut', 'heavenly', 'remove', 'boorish', 'frequent', 'card', 'fine', 'orange', 'well-made', 'awesome', 'approval', 'street', 'dizzy', 'collar', 'yielding', 'bent', 'cushion', 'habitual', 'zonked', 'better', 'fit', 'hand', 'glue', 'nappy'],
     timerDisplay: 0,
     timer: 0,
     interval: 0,
     score: 0,
-    bestScore: 0,
     gameDone: false,
-    gameType: "bubbles"
+    gameType: "bubbles",
+    topten: [],
+    url: 'http://localhost:3000', // 'http://localhost:3000', 'http://159.89.34.246:3000'
   },
   
   created() {
@@ -25,6 +27,24 @@ var app = new Vue({
         this.gameDone = false;
         this.gameOn = false;
       }
+    },
+    
+    showPopup: function(id) {
+      if (id == "topten") {
+        axios.get(this.url + "/api/scores/" + this.gameType).then(response => {
+          this.topten = response.data;
+          return true;
+        }).catch(err => {
+          console.log("error getting topten");
+        });
+      }
+      var popup = document.getElementById(id);
+      popup.style.display = "block";
+      var backdrop = document.getElementById("backdrop");
+      backdrop.style.display = "block";
+      var exit = document.getElementById("exitPopup");
+      exit.style.display = "block";
+      exit.onclick = "closePopup(id);"
     },
     
     popBubble: function(index) {
@@ -101,8 +121,33 @@ var app = new Vue({
         if (v.timer <= 0) {
           v.timer = 0;
           clearInterval(v.interval);
-          v.bestScore = Math.max(v.bestScore, v.score);
           v.gameDone = true;
+          
+          // Check high scores
+          axios.get(v.url + "/api/scores/" + v.gameType).then(response => {
+            console.log("a");
+            var topten = response.data;
+            if (topten.length == 0 || topten[topten.length-1].score < v.score || topten.length < 10) {
+              console.log("b");
+              var playerName = "";
+              var default1 = v.randomWords[v.getRandom(0,v.randomWords.length-1)];
+              var default2 = v.randomWords[v.getRandom(0,v.randomWords.length-1)];
+              while (playerName == "") {
+                playerName = prompt("Congrats! You made the top ten! Please enter in a name to go on the scoreboard:", default1 + " " + default2);
+              }
+              axios.post(v.url + "/api/scores/" + v.gameType, {
+              	name: playerName,
+              	score: v.score
+              }).then(response => {
+              	return true;
+              }).catch(err => {
+                console.log("error2");
+              });
+            }
+            return true;
+          }).catch(err => {
+            console.log("error1");
+          });
         }
         
         // Update display
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('public'))
+
+let bubblesScores = [];
+let fizzScores = [];
+let impactScores = [];
+let id = 0;
+
+app.get('/api/scores/:gameType', (req, res) => {
+  let gameType = req.params.gameType;
+  let scores = bubblesScores;
+  if (gameType == "fizz") {
+    scores = fizzScores;
+  } else if (gameType == "impact") {
+    scores = impactScores;
+  }
+  res.send(scores);
+});
+
+app.post('/api/scores/:gameType', (req, res) => {
+  let item = {name:req.body.name, score: req.body.score};
+  let gameType = req.params.gameType;
+  let scores = bubblesScores;
+  if (gameType == "fizz") {
+    scores = fizzScores;
+  } else if (gameType == "impact") {
+    scores = impactScores;
+  } 
+  if (scores.length == 0) {
+    scores.pop();
+  }
+  scores.push(item);
+  scores.sort(function(scoreItem1, scoreItem2){return scoreItem2.score-scoreItem1.score});
+  res.send(item);
+});
+
+app.listen(3000, () => console.log('Server listening on port 3000!'))
